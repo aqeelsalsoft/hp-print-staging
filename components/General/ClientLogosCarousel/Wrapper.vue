39cd54ba99b1ce34f8a2874d2248b0c3ce39e4e7
aqeelsalsoft <aqeel.shahid@salsoft.net>
2024-12-24 19:19:13 +0500

    Homepage Logos Carousel Updated

--- components/General/ClientLogosCarousel/Wrapper.vue
+++ components/General/ClientLogosCarousel/Wrapper.vue
@@ -1,18 +1,16 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
     <div class="hpp__clientLogosCarouselWrapper py-[30px]">
-        <div class="container">
+        <div class="container mx-auto px-[15px]">
             <div class="section__header mb-[30px]">
                 <h2 class="font-headings font-bold text-black text-[33px] mb-0 text-center">Trusted Name in The Best Online Printing Services in Dubai</h2>
             </div>
-            <GeneralClinetLogosCarouselItemSlider />
+            <ClientOnly>
+                <GeneralClientLogosCarouselItemSlider />
+            </ClientOnly>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
